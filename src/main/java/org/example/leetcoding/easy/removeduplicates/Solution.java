e4f704dbf83c2ffac338ec1e52592d0ac92618e9
mrallen <joshallen@outlook.com>
2020-12-15 14:55:46 -0800

    refactor: cleaner solution

--- src/main/java/org/example/leetcoding/easy/removeduplicates/Solution.java
+++ src/main/java/org/example/leetcoding/easy/removeduplicates/Solution.java
@@ -2,25 +2,14 @@ package org.example.leetcoding.easy.removeduplicates;
 
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int i = 0, j = 0;
-        while (i < nums.length) {
-            if (i == 0) {
+        if (nums == null || nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
                 i++;
-            } else {
-                if (nums[i] == nums[i - 1]) {
-                    i++;
-                } else {
-                    if (nums[j] != nums[i]) {
-                        j++;
-                        nums[j] = nums[i];
-                        i++;
-                    }
-                }
+                nums[i] = nums[j];
             }
         }
-        return j + 1;
+        return i + 1;
     }
 }
